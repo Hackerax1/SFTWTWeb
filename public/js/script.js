84e3090fe7f06d7bfbe5e795ebdb887392022702
Jack Banasiak <jbanasiak03@gmail.com>
2023-05-16 12:56:35 -0400

    got rid of redundant code

--- public/js/script.js
+++ public/js/script.js
@@ -16,49 +16,6 @@ function addInput() {
     }
   }
   
-  // Function to send a GET request to the server and display the result
-  function getGames() {
-    // Get all the input values for VanityURLs
-    var inputs = document.getElementsByName('vanityURL');
-    var vanityURLs = [];
-    for (var i = 0; i < inputs.length; i++) {
-      if (inputs[i].value) {
-        vanityURLs.push(inputs[i].value);
-      }
-    }
-  
-    // Send a GET request to the server with the VanityURLs
-    fetch('/games?vanityURLs=' + vanityURLs.join(','))
-      .then(response => response.json())
-      .then(data => {
-        // Display the common games
-        var commonGames = document.getElementById('common-games');
-        commonGames.innerHTML = '';
-        if (data.length == 0) {
-          commonGames.innerHTML = '<p>No common games found.</p>';
-        } else {
-          for (var i = 0; i < data.length; i++) {
-            var game = document.createElement('p');
-            game.innerText = data[i];
-            commonGames.appendChild(game);
-          }
-        }
-      })
-      .catch(error => console.error(error));
-  }
-  
-  // Function to choose a random game from the common games list
-  function chooseRandomGame() {
-    var commonGames = document.getElementById('common-games');
-    var gamesList = commonGames.getElementsByTagName('p');
-    if (gamesList.length > 0) {
-      var randomIndex = Math.floor(Math.random() * gamesList.length);
-      var randomGame = gamesList[randomIndex].innerText;
-      var randomGameText = document.getElementById('random-game-text');
-      randomGameText.innerText = randomGame;
-    }
-  }
-  
   // Function to save the common games list to a text file
   function saveGamesList() {
     var commonGames = document.getElementById('common-games');
